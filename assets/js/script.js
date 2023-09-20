f2bc350f14d3d2df64526506345d63de6a0870cb
CatherineP37 <>
2023-09-20 09:47:05 +0000

    Add js to shuffle boxes in game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ const boxes = document.querySelectorAll('.box');
 let firstBox;
 let secondBox;
 let matchCounter = 0;
+
+boxes.forEach(function (box) {
+    let randomNumber = Math.floor(Math.random() * 20);
+    box.style.order = randomNumber;	
