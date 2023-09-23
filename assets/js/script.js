fbafdb1dff80598bf19bb198a564066121bdefa6
CatherineP37 <>
2023-09-23 16:05:32 +0000

    Add functions to show numbers on click and to find matches

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,37 @@ let numberShowing = false;
 let lock = false;
 let firstBox, secondBox;
 
+// Shuffle boxes
+
 boxes.forEach(function (box) {
     let randomNumber = Math.floor(Math.random() * 20);
     box.style.order = randomNumber;
 )};
 
+
+// Show number on click and find matches
+
+function showNumber() {
+    if (lock) return;
+    this.classList.add('show');
+
+    if (!numberShowing) {
+        numberShowing = true;
+        firstBox = this;
+        return;
+    }
+
+    secondBox = this;
+    numberShowing = false;
+
+    match();
+}
+
+function match() {
+    if (firstBox.innerHTML === secondBox.innerHTML) {
+        deactivateClick();
+        return;
+    }
+
+    hideNumber();
+}
